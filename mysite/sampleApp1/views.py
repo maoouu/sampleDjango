d33a35fc360488e1649ff30b162226ba9a236ba5
maoouu <33371589+maoouu@users.noreply.github.com>
2022-02-12 21:52:36 +0800

    uploaded changes

--- mysite/sampleApp1/views.py
+++ mysite/sampleApp1/views.py
@@ -3,6 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(response):
-    # This is how you load an html file from template
-    # render(HTMLResponse, <directory-to-file>)
     return render(response, "sampleApp1/index.html")
